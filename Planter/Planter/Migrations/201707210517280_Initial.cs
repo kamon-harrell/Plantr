a6518f72d1f7fa15dee41a75bda06e06b91489d7
Kamon Harrell <Kamon Harrell>
2017-07-20 23:25:23 -0600

    refactoring of planter backend, server is working with localDB

---
+++ Planter/Planter/Migrations/201707210517280_Initial.cs
@@ -0,0 +1,32 @@
+namespace Planter.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Plants",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                        Price = c.Single(nullable: false),
+                        Harvest = c.String(),
+                        Water = c.String(),
+                        Description = c.String(),
+                        Space = c.String(),
+                        Germination = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Plants");
+        }
+    }
+}
---
+++ Planter/Planter/Models/Plant.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Planter.Models
+{
+    public class Plant
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public string Harvest { get; set; }
+        public string Water { get; set; }
+        public string Description { get; set; }
+        public string Space { get; set; }
+        public int Germination { get; set; }
+
+    }
+}
